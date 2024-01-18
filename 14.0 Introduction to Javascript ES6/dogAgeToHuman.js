8baf07c0abf12a68278aeb829b5d0236bf8b480c
quyenducngo <quyenducngo@gmail.com>
2024-01-18 13:44:46 -0800

    dog to human exercise and numbers quiz

---
+++ 14.0 Introduction to Javascript ES6/dogAgeToHuman.js
@@ -0,0 +1,8 @@
+dogAge = prompt("How old is your dog?");
+// Prompt asking how old is your dog 
+
+//Formula for human to dog age
+humanAge = ((dogAge - 2) * 4) + 21;
+
+//Sentence
+alert("Your dog is " + humanAge + " years old in human years");
