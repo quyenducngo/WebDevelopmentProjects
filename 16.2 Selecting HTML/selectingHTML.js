27b67b10743e72ae5ba826eebda0c265a760cdae
quyenducngo <quyenducngo@gmail.com>
2024-01-26 14:29:50 -0800

    learning DOM

---
+++ 16.2 Selecting HTML/selectingHTML.js
@@ -0,0 +1,15 @@
+document.querySelector("#li a").style.color = "red";
+
+document.querySelector("button").backgroundColor = "yellow";
+
+document.body.style.backgroundColor = "red";
+
+document.getElementById("Title").innerHTML = "Good Bye";
+
+document.getElementById("Title").textContent = "Hello";
+
+
+// Attributes: 
+// - href
+// - type
+// - class
