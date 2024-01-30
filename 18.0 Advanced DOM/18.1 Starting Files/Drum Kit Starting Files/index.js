211745872b8ab31c2120fd778ec6667d49f3c3c1
quyenducngo <quyenducngo@gmail.com>
2024-01-30 13:38:28 -0800

    learning to add sounds and switch-case

--- 18.0 Advanced DOM/18.1 Starting Files/Drum Kit Starting Files/index.js
+++ 18.0 Advanced DOM/18.1 Starting Files/Drum Kit Starting Files/index.js
@@ -1,37 +1,21 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked");
-  });
-}
 
+    var buttonInnerHTML = this.innerHTML;
 
-//example
-// $0.addEventListener(intput1, input2);
-// function respondToClick() {
-//   console.log("I got clicked");
-// };
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
 
-function add(num1, num2) {
-  return num1 + num2;
-}
+      case "a":
+        var tom2 = new audio("sound/tom-2.mp3");
+        tom1.play();
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-function calculator(num1, num2, operator) {
-  return operator(num1, num2);
-}
-
-calculator(4,5,add);
-calculator(4,5,multiply);
-debugger;
-calculator(10,5,divide);
+      default:
+    }
+  })
+};
