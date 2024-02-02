e02f694054019df84beaea0f028d785741db7667
quyenducngo <quyenducngo@gmail.com>
2024-02-02 09:16:12 -0800

    first couple of lessons

---
+++ 20.0 Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+
+function nextSequence() {
+  //generate a random number between 0 - 3 
+  // store it in a variable called 
+  return randomNumber;
+}
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var randomChosenColor 
+//use the randomNumber to select a random colour from buttonColours
+
+var gamePattern = [];
+//add the new randomChosenColour to the gamePattern
