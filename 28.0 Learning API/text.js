c625b4bcd3ad2501869feb7f6e8c39a3d3087113
quyenducngo <quyenducngo@gmail.com>
2024-02-10 15:00:38 -0800

    structuring APIs

--- 28.0 Learning API/text.js
+++ 28.0 Learning API/text.js
@@ -1,2 +1,2 @@
-1. get request in postman: https://api.wheretheiss.at/v1/satellites
-2. get request in postman: https://api.wheretheiss.at/v1/satellites/25544
+// 1. get request in postman: https://api.wheretheiss.at/v1/satellites
+// 2. get request in postman: https://api.wheretheiss.at/v1/satellites/25544
