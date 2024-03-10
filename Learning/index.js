9c89df6592b4d09c5f93e2616d82142c778234f0
quyenducngo <quyenducngo@gmail.com>
2024-03-10 13:34:06 -0700

    learn some components

--- Learning/index.js
+++ Learning/index.js
@@ -1,20 +1,29 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Heading from "./Heading";
+
+// ReactDOM.render(
+//   <div>
+//     <Heading />
+//     <ul>
+//       <li>Bacon</li>
+//       <li>Jamon</li>
+//       <li>Noodles</li>
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+//CHALLENGE
 import React from "react";
 import ReactDOM from "react-dom";
-
-const fName = "Jimmy";
-const lName = "Ngo";
-const currentDate = new Date();
-// console.log(currentDate);
-const year = currentDate.getFullYear();
-// const luckyNumber = 8;
+import Heading from "./Heading";
+import List from "./List";
 
 ReactDOM.render(
   <div>
-    <p className="heading">Created by {fName} {lName}</p>
-    <p>Copyright {year}</p>
+    <Heading />
+    <List />
   </div>,
   document.getElementById("root")
 );
-
-// Adding CSS styling to React now
-
